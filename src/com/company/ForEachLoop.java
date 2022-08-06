039694864cf249def387b099519464d64f49ee74
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-06 16:36:32 +0500

    for each loop

---
+++ src/com/company/ForEachLoop.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ForEachLoop {
+
+    public static void Main(String[]args){
+
+        int [] intArray = { 10, 20, 30, 40, 50 };
+
+        for( int value : intArray ) {
+            System.out.println( value );
+        }
+
+    }
+}
